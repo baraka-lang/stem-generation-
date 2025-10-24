f68f44ad288f2b6264fec080871126e76fcfa6ad
menye94 <98473873+menye94@users.noreply.github.com>
2025-10-24 14:07:11 +0300

    After force push

--- src/Auth/profilePage.js
+++ src/Auth/profilePage.js
@@ -28,7 +28,7 @@ function setupEventListeners() {
 
       setTimeout(() => {
         if (window.showPage) {
-          window.showPage('/')
+          window.showPage('/#selection')
         }
       }, 50)
     })
